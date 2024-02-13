e2b3302713a1b6ffccffa11ecca1a0ac73459b77
six-shot <okhuomonajayi54@gmail.com>
2024-02-13 10:13:50 -0800

    nav

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -7,11 +7,12 @@ const Hero = () => {
       <Navbar />
       <div className="flex items-center h-[80vh]">
         <div>
+          <p className="sm:w-[600px] text-sm sm:text-base w-full mb-2 ">Welcome,</p>
           <h1 className="uppercase  sm:text-[50px] text-[30px] font-bold sm:leading-[60px] leading-[40px]">
-            Lorem ipsum dolor{" "}
-            <br className="sm:flex hidden" /> sit amet consectetur
+            Lorem ipsum dolor <br className="sm:flex hidden" /> sit amet
+            consectetur
           </h1>
-          <p className="sm:w-[600px] text-sm sm:text-base w-full my-5 sm:my-10">
+          <p className="sm:w-[600px] text-sm sm:text-base w-full my-4 sm:my-10">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
