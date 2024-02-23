1637e36389533cee46efd9855e11ffd2df2801d7
six-shot <okhuomonajayi54@gmail.com>
2024-02-23 02:06:48 -0800

    services

--- src/components/what-we-offer/page.js
+++ src/components/what-we-offer/page.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { RiSecurePaymentFill } from "react-icons/ri";
 
 export default function WhatWeOffer() {
   return (
@@ -14,6 +15,75 @@ export default function WhatWeOffer() {
           All our phone numbers are from the US and can be bought in countries
           including the UK, Russia, India, Canada, and France.
         </h5>
+      </div>
+      <div className="px-[4%] sm:px-[5%] mt-[6%]">
+        <div className="grid sm:grid-cols-3 grid-cols-1 gap-12">
+          <div>
+            <div className="w-[50px] h-[50px] bg-[#EF5242] flex justify-center items-center rounded-full">
+              <RiSecurePaymentFill className="text-[30px] text-white " />
+            </div>
+            <h4 className="text-[15px] mt-3 mb-2 capitalize font-semibold text-[#EF5242] ">
+              Bypass any text or voice verification.
+            </h4>
+            <p className="text-sm ">
+              We offer real non-voip US based phone numbers for all major online
+              platforms and mobile applications. Long and short term number
+              rentals available.
+            </p>
+          </div>
+          <div>
+            <div className="w-[50px] h-[50px] bg-[#EF5242] flex justify-center items-center rounded-full">
+              <RiSecurePaymentFill className="text-[30px] text-white " />
+            </div>
+            <h4 className="text-[15px] mt-3 mb-2 capitalize font-semibold text-[#EF5242] ">
+              Bypass any text or voice verification.
+            </h4>
+            <p className="text-sm ">
+              We offer real non-voip US based phone numbers for all major online
+              platforms and mobile applications. Long and short term number
+              rentals available.
+            </p>
+          </div>
+          <div>
+            <div className="w-[50px] h-[50px] bg-[#EF5242] flex justify-center items-center rounded-full">
+              <RiSecurePaymentFill className="text-[30px] text-white " />
+            </div>
+            <h4 className="text-[15px] mt-3 mb-2 capitalize font-semibold text-[#EF5242] ">
+              Bypass any text or voice verification.
+            </h4>
+            <p className="text-sm ">
+              We offer real non-voip US based phone numbers for all major online
+              platforms and mobile applications. Long and short term number
+              rentals available.
+            </p>
+          </div>
+          <div>
+            <div className="w-[50px] h-[50px] bg-[#EF5242] flex justify-center items-center rounded-full">
+              <RiSecurePaymentFill className="text-[30px] text-white " />
+            </div>
+            <h4 className="text-[15px] mt-3 mb-2 capitalize font-semibold text-[#EF5242] ">
+              Bypass any text or voice verification.
+            </h4>
+            <p className="text-sm ">
+              We offer real non-voip US based phone numbers for all major online
+              platforms and mobile applications. Long and short term number
+              rentals available.
+            </p>
+          </div>
+          <div>
+            <div className="w-[50px] h-[50px] bg-[#EF5242] flex justify-center items-center rounded-full">
+              <RiSecurePaymentFill className="text-[30px] text-white " />
+            </div>
+            <h4 className="text-[15px] mt-3 mb-2 capitalize font-semibold text-[#EF5242] ">
+              Bypass any text or voice verification.
+            </h4>
+            <p className="text-sm ">
+              We offer real non-voip US based phone numbers for all major online
+              platforms and mobile applications. Long and short term number
+              rentals available.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
