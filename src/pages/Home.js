8cb6d170b33560bb54908667d35a9aaa1376bab0
six-shot <okhuomonajayi54@gmail.com>
2024-02-28 06:04:04 -0800

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import BrandSlider from "../components/brandslider";
 import Feauture from "../components/feauture";
 import WhatWeOffer from "../components/what-we-offer/page";
 import FAQ from "../components/accordion/Faq";
+import Testimonial from "../components/testimonial";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Feauture/>
       <WhatWeOffer/>
       <FAQ/>
+      <Testimonial/>
     </div>
   );
 };
