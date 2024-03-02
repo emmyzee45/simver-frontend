81b4e391c2d580121e3f0fdba560255e310cd963
six-shot <okhuomonajayi54@gmail.com>
2024-03-02 11:17:03 -0800

    fix

---
+++ src/components/feauture/contents/messaging/Messaging.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Messaging() {
+  return (
+    <div>
+      <div className="">
+        <h3 className="capitalize text-2xl leading-[30px] mb-4">
+          Lorem Ipsum Dolor Sit Amet <br className="sm:flex hidden " />
+          consectetur Lorem Ipsum Dolor <br className="sm:flex hidden " /> Sit
+          Amet consectetur
+        </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        </p>
+        <p className="my-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        </p>
+      </div>
+    </div>
+  );
+}
